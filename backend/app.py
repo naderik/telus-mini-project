d1053c712ccde3cb3397943910513913689b5d4f
Kasra Naderi <kasra@yuser.co>
2022-10-05 23:20:29 -0400

    contracts migrated successfully

--- backend/app.py
+++ backend/app.py
@@ -1,94 +1,63 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-import os
+from db import insert_contract, get_contracts, create_contracts_table
 
 app = Flask(__name__)
 
-# Set up database cpnnection
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://flask-app:flaskapp@35.226.141.139:5432/contract-data"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-# create the contact model
-
-class Contract(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vndr_nm = db.Column(db.String(120), nullable=False)
-    vndr_num = db.Column(db.Integer(), nullable=False)
-    agmnt_num = db.Column(db.Integer(), unique=True, nullable=False)
-    cntrct_stat_cd = db.Column(db.String(120), nullable=False)
-    cntrct_catgy_cd = db.Column(db.String(120), nullable=False)
-    cntrct_eff_dt = db.Column(db.Date(), nullable=False)
-    cntrct_expir_dt = db.Column(db.Date(), nullable=False)
-
-    def __init__(self, vndr_nm, vndr_num, agmnt_num, cntrct_stat_cd, cntrct_catgy_cd, cntrct_eff_dt, cntrct_expir_dt):
-        self.vndr_nm = vndr_nm
-        self.vndr_num = vndr_num
-        self.agmnt_num = agmnt_num
-        self.cntrct_stat_cd = cntrct_stat_cd
-        self.cntrct_catgy_cd = cntrct_catgy_cd
-        self.cntrct_eff_dt = cntrct_eff_dt
-        self.cntrct_expir_dt = cntrct_expir_dt
-    
-    def __repr__(self):
-        return '<Contract %r>' % self.id
 
 @app.route('/')
 def index():
     return jsonify({"message": "Hello, World!"})
 
 # get single contract by id
-@app.route('/contracts/<id>', methods=['GET'])
-def get_contract(id):
-    contract = Contract.query.get(id)
-    del contract.__dict__['_sa_instance_state']
-    return jsonify(contract.__dict__)
-
 
 # get all contracts
+
+
 @app.route('/contracts', methods=['GET'])
-def get_contracts():
+def data():
+    '''
+    Function used to get all contracts from Postgres database and return to fetch call in frontend.
+    :return: Json format of either collected calculations or error message
+    '''
+
     all_contracts = []
-    contracts = Contract.query.all()
-    for contract in contracts:
-        del contract.__dict__['_sa_instance_state']
-        all_contracts.append(contract.__dict__)
-    return jsonify(all_contracts)
+
+    try:
+        contracts = get_contracts()
+        for key, value in contracts.items():
+            all_contracts.append(value)
+
+        return jsonify({'contracts': all_contracts}), 200
+    except:
+        return jsonify({'error': 'error fetching contracts'}), 500
+
 
 # create a new contract
+
 @app.route('/contracts', methods=['POST'])
-def create_contract():
-    data = request.get_json()
-    new_contract = Contract(vndr_nm=data['vndr_nm'], vndr_num=data['vndr_num'], agmnt_num=data['agmnt_num'], cntrct_stat_cd=data['cntrct_stat_cd'], cntrct_catgy_cd=data['cntrct_catgy_cd'], cntrct_eff_dt=data['cntrct_eff_dt'], cntrct_expir_dt=data['cntrct_expir_dt'])
-    db.session.add(new_contract)
-    db.session.commit()
-    return jsonify(new_contract)
+def add_contract():
+    '''
+        Function used to insert a calculation into our postgres
+        DB. Operands of operation received from frontend.
+    :return: Json format of either success or failure response.
+    '''
+
+    add_contract = request.get_json()
+    vndr_nm, vndr_num, agmnt_num, cntrct_stat_cd, cntrct_catgy_cd, cntrct_eff_dt, cntrct_expir_dt = add_contract['vndr_nm'], add_contract['vndr_num'], add_contract[
+        'agmnt_num'], add_contract['cntrct_stat_cd'], add_contract['cntrct_catgy_cd'], add_contract['cntrct_eff_dt'], add_contract['cntrct_expir_dt']
+
+    try:
+        insert_contract(vndr_nm, vndr_num, agmnt_num, cntrct_stat_cd,
+                        cntrct_catgy_cd, cntrct_eff_dt, cntrct_expir_dt)
+        return jsonify({'Response': 'Successfully inserted into DB'}), 200
+    except:
+        return jsonify({'Response': 'Unable to insert into DB'}), 500
 
 # update a contract
-@app.route('/contracts/<id>', methods=['PUT'])
-def update_contract(id):
-    data = request.get_json()
-    contract = Contract.query.get(id)
-    contract.vndr_nm = data['vndr_nm']
-    contract.vndr_num = data['vndr_num']
-    contract.agmnt_num = data['agmnt_num']
-    contract.cntrct_stat_cd = data['cntrct_stat_cd']
-    contract.cntrct_catgy_cd = data['cntrct_catgy_cd']
-    contract.cntrct_eff_dt = data['cntrct_eff_dt']
-    contract.cntrct_expir_dt = data['cntrct_expir_dt']
-    db.session.commit()
-    return jsonify(contract.__dict__)
 
 # delete a contract
-@app.route('/contracts/<id>', methods=['DELETE'])
-def delete_contract(id):
-    contract = Contract.query.get(id)
-    db.session.delete(contract)
-    db.session.commit()
-    return jsonify(contract.__dict__)
 
 
 if __name__ == '__main__':
+    create_contracts_table()
     app.run(debug=True)
---
+++ backend/db.py
@@ -0,0 +1,157 @@
+from configparser import ConfigParser
+from sqlalchemy import create_engine, exc
+import os
+
+def read_config():
+    '''
+        Instantiating a config file reader for our use case.
+        To be used to read credentials for connecting to PSQL
+        DB hosted in GCP SQL.
+    :return: ConfigParser
+    '''
+
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, '../config.ini')
+
+    config = ConfigParser()
+    config.read(file_path)
+
+    return config
+
+def connection_uri():
+    '''
+        Creating connection URI for connecting to PSQL DB. URI will
+        be used to create SQLAlchemy engine for executing queries.
+        :return: URI for our PSQL DB hosted in GCP SQL
+    '''
+    # config = read_config()
+
+    # URI = 'postgresql+psycopg2://{}:{}@/{}?host={}'.format(
+    #     config['history_database']['user'],
+    #     config['history_database']['password'],
+    #     config['history_database']['dbname'],
+    #     config['history_database']['host']
+    # )
+    URI = "postgresql://postgres:postgres@35.226.141.139:5432/contract-data"
+
+    return URI
+
+def create_contracts_table(): 
+    URI = connection_uri()
+    my_connection = None
+    TABLE_NAME = "contracts"
+
+    CREATE_TABLE_QUERY = """
+                    CREATE TABLE IF NOT EXISTS {} (
+                        vndr_nm VARCHAR(120) NOT NULL,
+                        vndr_num VARCHAR(120) NOT NULL,
+                        agmnt_num VARCHAR(120) NOT NULL,
+                        cntrct_stat_cd VARCHAR(120) NOT NULL,
+                        cntrct_catgy_cd VARCHAR(120) NOT NULL,
+                        cntrct_eff_dt VARCHAR(120) NOT NULL,
+                        cntrct_expir_dt VARCHAR(120) NOT NULL
+                    )""".format(TABLE_NAME)
+    
+    try:
+        engine = create_engine(URI, echo=False)
+        my_connection = engine.connect()
+        my_connection.execute(CREATE_TABLE_QUERY)
+
+        return "Table created successfully"
+
+    except exc.SQLAlchemyError as error:
+        return 'Error trying to create table: {}'.format(error)
+
+    finally:
+        my_connection.close()
+        engine.dispose()
+
+def insert_contract(vndr_nm, vndr_num, agmnt_num, cntrct_stat_cd, cntrct_catgy_cd, cntrct_eff_dt, cntrct_expir_dt):
+    '''
+    Function used to insert our contract into the DB.
+    :param vndr_nm: Vendor name
+    :param vndr_num: Vendor number
+    :param agmnt_num: Agreement number
+    :param cntrct_stat_cd: Contract status code
+    :param cntrct_catgy_cd: Contract category code
+    :param cntrct_eff_dt: Contract effective date
+    :param cntrct_expir_dt: Contract expiration date
+    :return: error or success strings for inserting into DB.
+    '''
+    URI = connection_uri()
+    my_connection = None
+
+    try:
+        engine = create_engine(URI, echo=True)
+        my_connection = engine.connect()
+
+        my_connection.execute('INSERT INTO contracts VALUES (%s, %s, %s, %s,  %s, %s, %s,)', (vndr_nm, vndr_num, agmnt_num, cntrct_stat_cd, cntrct_catgy_cd, cntrct_eff_dt, cntrct_expir_dt))
+        return "Insertion successful"
+
+    except exc.SQLAlchemyError as err:
+        return 'Error occured inserting into table {}. Exception: {}'.format("contracts", err)
+
+    finally:
+        my_connection.close()
+        engine.dispose()
+
+def get_contracts():
+    '''
+    Function used to fetch all contracts from PSQL DB.
+    :return: hashmap of contracts
+    '''
+
+    URI = connection_uri()
+    my_connection = None
+    
+    GET_CONTRACTS_QUERY = """
+                                SELECT * FROM contracts
+                             """
+
+    try:
+        engine = create_engine(URI, echo=False)
+        my_connection = engine.connect()
+
+        contracts = my_connection.execute(GET_CONTRACTS_QUERY)
+
+        all_contracts = {}
+
+        i = 1
+        for row in contracts:
+            all_contracts[i] = (row['vndr_nm'], row['vndr_num'], row['agmnt_num'], row['cntrct_stat_cd'], row['cntrct_catgy_cd'], row['cntrct_eff_dt'], row['cntrct_expir_dt'])
+            i += 1
+
+        return all_contracts
+
+    except exc.SQLAlchemyError as err:
+        return 'Error fetching from table {}. Exception: {}'.format("contracts", err)
+
+    finally:
+        my_connection.close()
+        engine.dispose()
+
+
+def delete_contract(id):
+    '''
+    Function used to delete contract fromDB, based on ID.
+    :param id: first operand
+    :return: string message on whether deleted successfully or not
+    '''
+
+    URI = connection_uri()
+    my_connection = None
+
+    try:
+        engine = create_engine(URI, echo=False)
+        my_connection = engine.connect()
+        my_connection.execute('DELETE from contracts WHERE id = {}'.format(id))
+        return "Deletion successful"
+    except exc.SQLAlchemyError as err:
+        return 'Error deleting data from table {}. Exception: {}'.format("contracts", err)
+
+    finally:
+        my_connection.close()
+        engine.dispose()
+
+if __name__=="__main__":
+    delete_contract(12345)
