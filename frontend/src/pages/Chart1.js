8f3b6b5f17b5d5d7c30d51038707343e09957925
Kasra Naderi <kasra@yuser.co>
2022-10-06 15:36:25 -0400

    app works

--- frontend/src/pages/Chart1.js
+++ frontend/src/pages/Chart1.js
@@ -1,24 +1,29 @@
 import React from "react";
 import BarChart from "../components/BarChart";
 
-var chartData = [];
+const tempData = []
 
-// check if the object with the same status exists in the array and if it does, increment the count by 1, otherwise add a new object to the array
-const countStatus = (status) => {
-    let found = false;
-    chartData.forEach((item) => {
-        if (item.status === status) {
-            item.count++;
-            found = true;
-        }
-    });
-    if (!found) {
-        chartData.push({ status: status, count: 1 });
-    }
-}
 
 function Chart1() {
     const [state, setState] = React.useState()
+    const [chartData, setChartData] = React.useState([])
+
+    // check if the object with the same status exists in the array and if it does, increment the count by 1, otherwise add a new object to the array
+    const countStatus = (status) => {
+        let found = false;
+        tempData.forEach((item) => {
+            if (item.status === status) {
+                item.count++;
+                found = true;
+            }
+        });
+        if (!found) {
+            tempData.push({ status: status, count: 1 });
+        }
+    }
+
+
+
 
     React.useEffect(() => {
         fetch("http://localhost:8585/api/contracts", {
@@ -33,12 +38,18 @@ function Chart1() {
         })
     }, [])
 
-    for (var i = 0; i < state?.data?.length; i++) {
-        countStatus(state?.data?.[i][4]);
-    }
+    React.useEffect(() => {
+
+        for (var i = 0; i < state?.data?.length; i++) {
+            countStatus(state?.data?.[i][4]);
+        }
+        setChartData(tempData)
+    }, [state])
+
+
+
 
     console.log(chartData)
-    console.log(state?.data)
     return (
         <div>
             <h1>Chart1</h1>
--- frontend/src/pages/Chart2.js
+++ frontend/src/pages/Chart2.js
@@ -1,60 +1,59 @@
 import React from "react";
-import BubbleChart from "@weknow/react-bubble-chart-d3";
-import ErrorBoundary from "../ErrorBoundary";
+import PieChart from "../components/PieChart";
+
+const tempData = []
+
+const data = [{ label: 'Apples', value: 10 }, { label: 'Oranges', value: 20 }];
+
+
 
 function Chart2() {
+    const [state, setState] = React.useState()
+    const [chartData, setChartData] = React.useState([])
+
+    const countNames = (name) => {
+        let found = false;
+        tempData.forEach((item) => {
+            if (item.label === name) {
+                item.value++;
+                found = true;
+            }
+        });
+        if (!found) {
+            tempData.push({ label: name, value: 1 });
+        }
+    }
+
+    React.useEffect(() => {
+        fetch("http://localhost:8585/api/contracts", {
+            method: 'GET',
+            mode: 'cors'
+        }).then(response => {
+            if (response.status === 200) {
+                response.json().then(data => {
+                    setState(data)
+                })
+            }
+        })
+    }, [])
+
+    React.useEffect(() => {
+
+        for (var i = 0; i < state?.data?.length; i++) {
+            countNames(state?.data?.[i][5]);
+        }
+        setChartData(tempData)
+    }, [state])
+
+
+
+
+    console.log(chartData)
+
     return (
         <>
-            <ErrorBoundary message="Problem in index.render: BubbleChart">
-                <BubbleChart
-                    graph={{
-                        zoom: 1.1,
-                        offsetX: -0.05,
-                        offsetY: -0.01,
-                    }}
-                    width={1000}
-                    height={800}
-                    padding={0} // optional value, number that set the padding between bubbles
-                    showLegend={true} // optional value, pass false to disable the legend.
-                    legendPercentage={20} // number that represent the % of with that legend going to use.
-                    legendFont={{
-                        family: 'Arial',
-                        size: 12,
-                        color: '#000',
-                        weight: 'bold',
-                    }}
-                    valueFont={{
-                        family: 'Arial',
-                        size: 12,
-                        color: '#fff',
-                        weight: 'bold',
-                    }}
-                    labelFont={{
-                        family: 'Arial',
-                        size: 16,
-                        color: '#fff',
-                        weight: 'bold',
-                    }}
-                    data={[
-                        { label: 'CRM', value: 1 },
-                        { label: 'API', value: 1 },
-                        { label: 'Data', value: 1 },
-                        { label: 'Commerce', value: 1 },
-                        { label: 'AI', value: 3 },
-                        { label: 'Management', value: 5 },
-                        { label: 'Testing', value: 6 },
-                        { label: 'Mobile', value: 9 },
-                        { label: 'Conversion', value: 9 },
-                        { label: 'Misc', value: 21 },
-                        { label: 'Databases', value: 22 },
-                        { label: 'DevOps', value: 22 },
-                        { label: 'Javascript', value: 23 },
-                        { label: 'Languages / Frameworks', value: 25 },
-                        { label: 'Front End', value: 26 },
-                        { label: 'Content', value: 26 },
-                    ]}
-                />
-            </ErrorBoundary>
+            <h1>Chart2</h1>
+            <PieChart data={chartData} />
         </>
     );
 }
